36653c7aaa85a0dc39229a0e8e1c9e7545463fa1
jepski <102801829+zafrul1@users.noreply.github.com>
2022-05-11 18:16:09 +0200

    Update Spiel.h

--- Uebungsblatt3/SS22_PRG2_Uebungsblatt_3_Tibongo_Hilfsdateien/Spiel.h
+++ Uebungsblatt3/SS22_PRG2_Uebungsblatt_3_Tibongo_Hilfsdateien/Spiel.h
@@ -1,15 +1,28 @@
 #ifndef SPIEL
 #define SPIEL 
 #include<iostream>
-
+#include "Aufgabe.h"
 
 class Spiel {
 
 private:
-
+	Aufgabe aktuelleraufgabe;   
 
 
 public:
+	int aktiverSpielstein = 0; 
+	friend void testSpiel();
+	void init();
+	void ausgabeLogo();
+	void ausgabeRahmen(); 
+	void ausgabeLegende(); 
+	void ausgabeSteuerung();
+	void spielSchleife();
+	void spielzug();
+	bool eingabeErlaubt(char input); 
+	void draw(void); 
+	Spiel(Aufgabe* aufgabe);
+	
 
 
 
@@ -20,4 +33,4 @@ public:
 
 
 
-#endif // !SPIEL
+#endif 
